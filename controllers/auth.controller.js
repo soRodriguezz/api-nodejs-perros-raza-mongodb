ed3854f6ae74790fe7dc75d04d24e80720a9d04f
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-03-22 20:40:21 -0300

    username and password required signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,43 +3,48 @@ const Role = require("../models/Role");
 const jwt = require("jsonwebtoken");
 
 exports.signin = async (req, res) => {
-    const userFound = await User.findOne({email: req.body.email}).populate("roles");
 
-    if(!userFound) return res.status(400).json({message: "User not found"});
+  const { email, password } = req.body;
+  
+  if(email === '' || password === '') return res.status(400).json({message: "All fields are required"});
 
-    const matchPassword = await User.comparePassword(req.body.password, userFound.password);
+  const userFound = await User.findOne({email}).populate("roles");
 
-    if(!matchPassword) return res.status(401).json({token: null, message: "Password incorrect"});
+  if(!userFound) return res.status(400).json({message: "User not found"});
 
-    const token = jwt.sign({id: userFound._id}, process.env.SECRET, {
-        expiresIn: 900,
-    });
+  const matchPassword = await User.comparePassword(password, userFound.password);
 
-    res.json({token});
+  if(!matchPassword) return res.status(401).json({token: null, message: "Password incorrect"});
+
+  const token = jwt.sign({id: userFound._id}, process.env.SECRET, {
+    expiresIn: 900,
+  });
+
+  return res.json({token});
 };
 
 exports.signup = async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if ( username === '' || email === '' ||  password === '') {
-      return res.status(400).json({message: "All fields are required"});
-    }
-  
-    const newUser = new User({
-      username,
-      email,
-      password: await User.encryptPassword(password),
-    });
-  
-    const role = await Role.find({ name: "user" });
-    newUser.roles = role[0]._id;
+  if ( username === '' || email === '' ||  password === '') {
+    return res.status(400).json({message: "All fields are required"});
+  }
 
-    const savedUser = await newUser.save();
-  
-    const token = jwt.sign({ id: savedUser._id }, process.env.SECRET, {
-      expiresIn: 900,
-    });
-  
-    res.status(200).json({ token });
+  const newUser = new User({
+    username,
+    email,
+    password: await User.encryptPassword(password),
+  });
+
+  const role = await Role.find({ name: "user" });
+  newUser.roles = role[0]._id;
+
+  const savedUser = await newUser.save();
+
+  const token = jwt.sign({ id: savedUser._id }, process.env.SECRET, {
+    expiresIn: 900,
+  });
+
+  res.status(200).json({ token });
 };
 
